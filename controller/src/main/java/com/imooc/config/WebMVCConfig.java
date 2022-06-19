2ae2dbc68cf33b7baaba224c972211655e9273e6
mumabinggan <mumabinggan@163.com>
2022-06-19 13:27:41 +0800

    升级工程

--- controller/src/main/java/com/imooc/config/WebMVCConfig.java
+++ controller/src/main/java/com/imooc/config/WebMVCConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,8 +12,18 @@ public class WebMVCConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/**")
-				.addResourceLocations("file:/Users/muma/Images/");
+//		registry.addResourceHandler("/**")
+//				.addResourceLocations("file:/Users/muma/Images/");
+		registry.
+				addResourceHandler("/swagger-ui/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/springfox-swagger-ui/")
+				.resourceChain(false);
+	}
+
+	@Override
+	public void addViewControllers(ViewControllerRegistry registry) {
+		registry.addViewController("/swagger-ui/")
+				.setViewName("forward:/swagger-ui/index.html");
 	}
 
 	public RestTemplate restTemplate(RestTemplateBuilder builder) {
